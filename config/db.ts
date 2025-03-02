49abb27f61b03744e2f43e53c3865e0cb479d8ce
xPedroL <pedrolucassantosferreira426@gmail.com>
2025-03-02 19:21:22 -0300

    ajuste ao conectar ao banco.

--- config/db.ts
+++ config/db.ts
@@ -1,13 +1,29 @@
-import mongoose from 'mongoose'
+import mongoose, { ConnectOptions } from 'mongoose'
+
 const { MONGODB_URI } = process.env
-export const connectDB = async (): Promise<boolean | undefined> => {
+
+export const connectDB = async (): Promise<boolean> => {
   try {
-    const { connection } = await mongoose.connect(MONGODB_URI as string)
+    if (mongoose.connections[0]?.readyState === 1) {
+      return true
+    }
+
+    const options: ConnectOptions = {
+      maxPoolSize: 10,
+      serverSelectionTimeoutMS: 5000,
+      socketTimeoutMS: 45000,
+    }
+    const { connection } = await mongoose.connect(
+      MONGODB_URI as string,
+      options,
+    )
+
     if (connection.readyState === 1) {
-      return Promise.resolve(true)
+      return true
+    } else {
+      throw new Error('Failed to establish database connection')
     }
   } catch (error) {
-    console.error(error)
-    return Promise.reject(error)
+    throw error
   }
 }
