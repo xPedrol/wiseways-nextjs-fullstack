029e108e3b3a6023e74c1874b4a9c92307e1cf8c
xPedroL <pedrolucassantosferreira426@gmail.com>
2025-03-15 12:37:42 -0300

    cssnano

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -2,7 +2,8 @@
 const config = {
   plugins: {
     tailwindcss: {},
+    ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {}),
   },
-};
+}
 
-export default config;
+export default config
