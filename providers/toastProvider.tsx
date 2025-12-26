c4168a77a331b55a9bdbc8afc743e00d45b8e3a9
Pedro Lucas Santos Ferreira <pedrolucassantosferreira426@gmail.com>
2025-12-26 11:44:29 -0300

    ajustes de versões

--- providers/toastProvider.tsx
+++ providers/toastProvider.tsx
@@ -1,58 +1,57 @@
-'use client'
-import { Ban, CircleCheckBig, Info } from 'lucide-react'
-import style from './toast.module.scss'
-import { createContext, useContext, useRef, useState } from 'react'
+"use client";
+import { Ban, CircleCheckBig, Info } from "lucide-react";
+import style from "./toast.module.scss";
+import { createContext, useContext, useRef, useState } from "react";
 
 type Toast = {
-  message: string
-  type: 'success' | 'error' | 'info'
-}
-const getToastIcon = (type: Toast['type']) => {
+  message: string;
+  type: "success" | "error" | "info";
+};
+const getToastIcon = (type: Toast["type"]) => {
   switch (type) {
-    case 'success':
-      return <CircleCheckBig size={30} />
-    case 'error':
-      return <Ban size={30} />
-    case 'info':
-      return <Info size={30} />
+    case "success":
+      return <CircleCheckBig size={30} />;
+    case "error":
+      return <Ban size={30} />;
+    case "info":
+      return <Info size={30} />;
     default:
-      return ''
+      return "";
   }
-}
+};
 type Context = {
-  showToast: (message: Toast['message'], type: Toast['type']) => void
-  hideToast: () => void
-}
+  showToast: (message: Toast["message"], type: Toast["type"]) => void;
+  hideToast: () => void;
+};
 const ToastContext = createContext<Context>({
-  showToast: (message: Toast['message'], type: Toast['type']) => {},
+  showToast: (message: Toast["message"], type: Toast["type"]) => {},
   hideToast: () => {},
-})
+});
 type Props = {
-  children: React.ReactNode
-}
+  children: React.ReactNode;
+};
 export const ToastProvider = ({ children }: Props) => {
-  const queue = useRef<Toast[]>([])
-  const [key, setKey] = useState(0)
+  const queue = useRef<Toast[]>([]);
+  const [key, setKey] = useState(0);
   const handleForceUpdate = () => {
-    setKey((prevKey) => prevKey + 1)
-  }
-  const showToast = (message: Toast['message'], type: Toast['type']) => {
-    queue.current.push({ message, type })
-    handleForceUpdate()
-    console.log(queue)
+    setKey((prevKey) => prevKey + 1);
+  };
+  const showToast = (message: Toast["message"], type: Toast["type"]) => {
+    queue.current.push({ message, type });
+    handleForceUpdate();
     setTimeout(() => {
-      hideToast()
-    }, 3000)
-  }
+      hideToast();
+    }, 3000);
+  };
   const hideToast = () => {
-    queue.current.shift()
-    console.log(queue)
-    handleForceUpdate()
-  }
+    queue.current.shift();
+    console.log(queue);
+    handleForceUpdate();
+  };
   const value = {
     showToast,
     hideToast,
-  }
+  };
   return (
     <ToastContext.Provider value={value}>
       <div className={style.container}>
@@ -65,13 +64,13 @@ export const ToastProvider = ({ children }: Props) => {
       </div>
       {children}
     </ToastContext.Provider>
-  )
-}
+  );
+};
 
 export const useToast = () => {
-  const context = useContext(ToastContext)
+  const context = useContext(ToastContext);
   if (!context) {
-    throw new Error('useToast must be used within a ToastProvider')
+    throw new Error("useToast must be used within a ToastProvider");
   }
-  return context
-}
+  return context;
+};
